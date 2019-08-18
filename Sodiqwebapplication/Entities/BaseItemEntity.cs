4b17e9272caa0d739d3a07e70fb7fd9db331f0f9
Simpa Saiki <simpasaiki01@gmail.com>
2019-08-18 03:26:35 +0100

    entity modifications

---
+++ Sodiqwebapplication/Entities/BaseItemEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sodiqwebapplication.Entities
+{
+    public class BaseItemEntity
+    {
+        public BaseItemEntity()
+        {
+        }
+        [Key]
+        public long Id { get; set; }
+        public long ItemId { get; set; }
+
+        public String Type { get; set; }
+        public String SubType { get; set; }
+        public String Author { get; set; }
+        public String Title { get; set; }
+        public DateTime Date { get; set; }
+        public String Summary { get; set; }
+        public String Content { get; set; }
+        public String Url { get; set; }
+        public String IsFeatured { get; set; }
+        public String SmallThumbnail { get; set; }
+        public String Thumbnail { get; set; }
+        public String OriginalThumbnail { get; set; }
+        public String LargeThumbnail { get; set; }
+
+        public String XLargeThumbnail { get; set; }
+        public String XxLargeThumbnail { get; set; }
+        public String Latitude { get; set; }
+        public String Longitude { get; set; }
+        public int IsHeadLine { get; set; }
+        public String AuthorAvatarUrl { get; set; }
+        public String Address { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public String PinIconColor { get; set; }
+        public String PinIconUrl { get; set; }
+        public int PinIconWidth { get; set; }
+        public int PinIconHeight { get; set; }
+        public String WebSite { get; set; }
+        public Boolean CommentsEnabled { get; set; }
+
+
+        public String SubSections { get; set; }
+    }
+}
---
+++ Sodiqwebapplication/Entities/OldImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sodiqwebapplication.Entities
+{
+    public class OldImage
+    {
+        public OldImage()
+        {
+        }
+
+        [Key]
+        public long Id { get; set; }
+        public String ImageId { get; set; }
+        public String Url { get; set; }
+        public String OtherImagesUrl { get; set; }
+        public OldItem OldItem { get; set; }
+    }
+}
